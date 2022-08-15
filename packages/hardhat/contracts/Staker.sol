62152d696e1820b69a977377b37ab36874502f7d
giggling-ginger <giggling.ginger1058@gmail.com>
2022-08-15 15:20:48 +0800

    1st commit

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,31 +5,182 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public depositTimestamps;
 
-  constructor(address exampleExternalContractAddress) {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    uint256 public constant rewardRatePerBlock = 0.1 ether;
+    uint256 public withdrawalDeadline = block.timestamp + 120 seconds;
+    uint256 public claimDeadline = block.timestamp + 240 seconds;
+    uint256 public currentBlock = 0;
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    // only "white-list" a single address
+    address public contractWhitelisted;
 
+    // Events
+    event Stake(address indexed sender, uint256 amount);
+    event Received(address, uint);
+    event Execute(address indexed sender, uint256 amount);
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+    // Modifiers
+    /*
+  Checks if the withdrawal period has been reached or not
+  */
+    modifier withdrawalDeadlineReached(bool requireReached) {
+        uint256 timeRemaining = withdrawalTimeLeft();
+        if (requireReached) {
+            require(timeRemaining == 0, "Withdrawal period is not reached yet");
+        } else {
+            require(timeRemaining > 0, "Withdrawal period has been reached");
+        }
+        _;
+    }
 
+    /*
+  Checks if the claim period has ended or not
+  */
+    modifier claimDeadlineReached(bool requireReached) {
+        uint256 timeRemaining = claimPeriodLeft();
+        if (requireReached) {
+            require(timeRemaining == 0, "Claim deadline is not reached yet");
+        } else {
+            require(timeRemaining > 0, "Claim deadline has been reached");
+        }
+        _;
+    }
 
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function
+    /*
+  Requires that the contract only be completed once!
+  */
+    modifier notCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "Stake already completed!");
+        _;
+    }
 
+    constructor(address exampleExternalContractAddress) {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
+        contractWhitelisted = exampleExternalContractAddress;
+    }
 
-  // Add a `withdraw()` function to let users withdraw their balance
+    // Stake function for a user to stake ETH in our contract
+    function stake(uint amount)
+        public
+        payable
+        withdrawalDeadlineReached(false)
+        claimDeadlineReached(false)
+    {
+        balances[msg.sender] = balances[msg.sender] + amount;
+        depositTimestamps[msg.sender] = block.timestamp;
+        emit Stake(msg.sender, amount);
+    }
 
+    /*
+  Withdraw function for a user to remove their staked ETH inclusive
+  of both principal and any accrued interest
+  */
+    function withdraw()
+        public
+        withdrawalDeadlineReached(true)
+        claimDeadlineReached(false)
+        notCompleted
+    {
+        require(balances[msg.sender] > 0, "You have no balance to withdraw!");
+        uint256 individualBalance = balances[msg.sender];
+        uint256 indBalanceRewards = individualBalance *
+            (1 + rewardRatePerBlock) **
+                (block.timestamp - depositTimestamps[msg.sender]);
+        balances[msg.sender] = 0;
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+        // Transfer all ETH via call! (not transfer) cc: https://solidity-by-example.org/sending-ether
+        (bool sent, bytes memory data) = msg.sender.call{
+            value: indBalanceRewards
+        }("");
+        require(sent, "RIP; withdrawal failed :( ");
+    }
 
+    /*
+  Allows any user to repatriate "unproductive" funds that are left in the staking contract
+  past the defined withdrawal period
+  */
+    function execute() public claimDeadlineReached(true) notCompleted {
+        uint256 contractBalance = address(this).balance;
+        exampleExternalContract.complete{value: address(this).balance}();
+    }
 
-  // Add the `receive()` special function that receives eth and calls stake()
+    // Functions added for reset:
 
+    /*
+  Reset withdrawalDeadline blocktime
+  Requires withdrawalDeadlineReached to be true
+  */
+    function resetWithdrawalDeadline(uint256 _seconds)
+        public
+        withdrawalDeadlineReached(true)
+    {
+        require(
+            msg.sender == contractWhitelisted,
+            "You are not the exampleExternalContract"
+        );
+        withdrawalDeadline = block.timestamp + (_seconds * 1 seconds);
+    }
 
+    /*
+  Reset claimDeadline blocktime
+  Requires claimDeadlineReached to be true
+  */
+    function resetClaimDeadline(uint256 _seconds)
+        public
+        claimDeadlineReached(true)
+    {
+        require(
+            msg.sender == contractWhitelisted,
+            "You are not the exampleExternalContract"
+        );
+        claimDeadline = block.timestamp + (_seconds * 1 seconds);
+    }
+
+    /*
+  READ-ONLY function to calculate the time remaining before the minimum staking period has passed
+  */
+    function withdrawalTimeLeft()
+        public
+        view
+        returns (uint256 withdrawalTimeLeft)
+    {
+        if (block.timestamp >= withdrawalDeadline) {
+            return (0);
+        } else {
+            return (withdrawalDeadline - block.timestamp);
+        }
+    }
+
+    /*
+  READ-ONLY function to calculate the time remaining before the minimum staking period has passed
+  */
+    function claimPeriodLeft() public view returns (uint256 claimPeriodLeft) {
+        if (block.timestamp >= claimDeadline) {
+            return (0);
+        } else {
+            return (claimDeadline - block.timestamp);
+        }
+    }
+
+    /*
+  Time to "kill-time" on our local testnet
+  */
+    function killTime() public {
+        currentBlock = block.timestamp;
+    }
+
+    /*
+  \Function for our smart contract to receive ETH
+  cc: https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function
+  */
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
 }
